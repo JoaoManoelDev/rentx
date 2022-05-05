8bcdf5f27e3794508594f47ea65613b14dac0639
“Joao <joaomanoeldev@gmail.com>
2022-05-04 23:15:14 -0300

    creating the api with nodejs

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express"
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository"
+
+const categoriesRoutes = Router()
+
+const categoriesRepository = new CategoriesRepository()
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name)
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "category already exists" })
+  }
+
+  categoriesRepository.create({ name, description })
+
+  return response.status(201).json({ message: "category created successfully" })
+})
+
+categoriesRoutes.get("/", (request, response) => {
+
+  const categories = categoriesRepository.list()
+
+  return response.status(200).json(categories)
+
+})
+
+export { categoriesRoutes }
